33099f498fcb8c0d20d0e803a752544f5d3dfc3f
SD5th <vlaka@ro.ru>
2024-11-05 01:37:05 +0300

    Added annotations

--- 3Sem/LW_3sem-1/ShrdPtr.cpp
+++ 3Sem/LW_3sem-1/ShrdPtr.cpp
@@ -1,151 +1,166 @@
 #include "ShrdPtr.h"
 
-template <class T>
-ShrdPtr<T>::ShrdPtr():
-	ptr(nullptr),
-  referenceCount(nullptr)
-{ }
+/* UnqPtr to object */
+/* Getters and setters */
 
-template <class T>
-ShrdPtr<T>::ShrdPtr(T* const & ptr):
-	ptr(ptr),
-  referenceCount(new unsigned int(1))
-{ }
+/* Constructors */
+  // ShrdPtr with nullptr
+  template <class T>
+  ShrdPtr<T>::ShrdPtr():
+    ptr(nullptr),
+    referenceCount(nullptr)
+  { }
 
-template <class T>
-ShrdPtr<T>::ShrdPtr(T const & object):
-	ptr(new T(object)),
-  referenceCount(new unsigned int(1))
-{ }
+  // ShrdPtr with given ptr
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(T* const & ptr):
+    ptr(ptr),
+    referenceCount(new unsigned int(1))
+  { }
 
-template <class T>
-ShrdPtr<T>::ShrdPtr(ShrdPtr const & shrdPtr):
-  ptr(shrdPtr.ptr),
-  referenceCount(shrdPtr.referenceCount),
-  isArray(shrdPtr.isArray) 
-{
-  *referenceCount += 1;
-} 
+  // ShrdPtr to copy of given object
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(T const & object):
+    ptr(new T(object)),
+    referenceCount(new unsigned int(1))
+  { }
 
-template <class T>
-ShrdPtr<T>::~ShrdPtr()
-{
-  *referenceCount -= 1;
-	if (*referenceCount == 0)
+  // share ptr
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(ShrdPtr const & shrdPtr):
+    ptr(shrdPtr.ptr),
+    referenceCount(shrdPtr.referenceCount),
+    isArray(shrdPtr.isArray) 
   {
-    if (isArray)
-		  delete ptr;
-	  else
-  		delete[] ptr;
+    *referenceCount += 1;
   }
-}
 
-template <class T>
-T& ShrdPtr<T>::operator*() {
-  return *ptr;
-}
-
-template <class T>
-const T& ShrdPtr<T>::operator*() const {
-  return *ptr;
-}
-
-template <class T>
-T& ShrdPtr<T>::operator[](size_t const & index) {
-  return ptr[index];
-}
-
-template <class T>
-const T& ShrdPtr<T>::operator[](size_t const & index) const {
-  return ptr[index];
-}
-
-template <class T>
-ShrdPtr<T> ShrdPtr<T>::operator=(ShrdPtr const &)
-{
-  return ShrdPtr(this);
-}
-
-
-
-
-
-
-
-
-
-template <class T>
-ShrdPtr<T>::ShrdPtr():
-	ptr(nullptr),
-  referenceCount(new unsigned int(1))
-{ }
-
-template <class T>
-ShrdPtr<T>::ShrdPtr(T* const & ptr):
-	ptr(ptr),
-  referenceCount(new unsigned int(1))
-{ }
-
-template <class T>
-ShrdPtr<T>::ShrdPtr(T const & object):
-	ptr(new T(object)),
-  referenceCount(new unsigned int(1))
-{ }
-
-template <class T>
-ShrdPtr<T>::ShrdPtr(T* const & array, int const & arrSize):
-	ptr(new T[arrSize]),
-  referenceCount(new int(1)),
-  isArray(true)
-{
-  if (array != nullptr)
-    std::memcpy(ptr, array, arrSize * sizeof(T));
-}
-
-template <class T>
-ShrdPtr<T>::ShrdPtr(ShrdPtr const & shrdPtr):
-  ptr(shrdPtr.ptr),
-  referenceCount(shrdPtr.referenceCount),
-  isArray(shrdPtr.isArray) 
-{
-  *referenceCount += 1;
-} 
-
-template <class T>
-ShrdPtr<T>::~ShrdPtr()
-{
-  *referenceCount -= 1;
-	if (*referenceCount == 0)
+/* Destructor */
+  template <class T>
+  ShrdPtr<T>::~ShrdPtr()
   {
-    if (isArray)
-		  delete ptr;
-	  else
-  		delete[] ptr;
+    *referenceCount -= 1;
+    if (*referenceCount == 0)
+    {
+      if (isArray)
+        delete ptr;
+      else
+        delete[] ptr;
+    }
   }
-}
 
-template <class T>
-T& ShrdPtr<T>::operator*() {
-  return *ptr;
-}
+/* Operators */
+  template <class T>
+  T& ShrdPtr<T>::operator*() {
+    return *ptr;
+  }
 
-template <class T>
-const T& ShrdPtr<T>::operator*() const {
-  return *ptr;
-}
+  template <class T>
+  const T& ShrdPtr<T>::operator*() const {
+    return *ptr;
+  }
 
-template <class T>
-T& ShrdPtr<T>::operator[](size_t const & index) {
-  return ptr[index];
-}
+  template <class T>
+  T& ShrdPtr<T>::operator[](size_t const & index) {
+    return ptr[index];
+  }
 
-template <class T>
-const T& ShrdPtr<T>::operator[](size_t const & index) const {
-  return ptr[index];
-}
+  template <class T>
+  const T& ShrdPtr<T>::operator[](size_t const & index) const {
+    return ptr[index];
+  }
 
-template <class T>
-ShrdPtr<T> ShrdPtr<T>::operator=(ShrdPtr const &)
-{
-  return ShrdPtr(this);
-}
+  template <class T>
+  ShrdPtr<T> ShrdPtr<T>::operator=(ShrdPtr const &)
+  {
+    return ShrdPtr(this);
+  }
+
+
+
+
+
+
+
+
+/* UnqPtr to array */
+/* Getters and setters */
+
+/* Constructors */
+  template <class T>
+  ShrdPtr<T>::ShrdPtr():
+    ptr(nullptr),
+    referenceCount(new unsigned int(1))
+  { }
+
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(T* const & ptr):
+    ptr(ptr),
+    referenceCount(new unsigned int(1))
+  { }
+
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(T const & object):
+    ptr(new T(object)),
+    referenceCount(new unsigned int(1))
+  { }
+
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(T* const & array, int const & arrSize):
+    ptr(new T[arrSize]),
+    referenceCount(new int(1)),
+    isArray(true)
+  {
+    if (array != nullptr)
+      std::memcpy(ptr, array, arrSize * sizeof(T));
+  }
+
+  template <class T>
+  ShrdPtr<T>::ShrdPtr(ShrdPtr const & shrdPtr):
+    ptr(shrdPtr.ptr),
+    referenceCount(shrdPtr.referenceCount),
+    isArray(shrdPtr.isArray) 
+  {
+    *referenceCount += 1;
+  } 
+
+/* Destructor */
+  template <class T>
+  ShrdPtr<T>::~ShrdPtr()
+  {
+    *referenceCount -= 1;
+    if (*referenceCount == 0)
+    {
+      if (isArray)
+        delete ptr;
+      else
+        delete[] ptr;
+    }
+  }
+
+/* Operators */
+  template <class T>
+  T& ShrdPtr<T>::operator*() {
+    return *ptr;
+  }
+
+  template <class T>
+  const T& ShrdPtr<T>::operator*() const {
+    return *ptr;
+  }
+
+  template <class T>
+  T& ShrdPtr<T>::operator[](size_t const & index) {
+    return ptr[index];
+  }
+
+  template <class T>
+  const T& ShrdPtr<T>::operator[](size_t const & index) const {
+    return ptr[index];
+  }
+
+  template <class T>
+  ShrdPtr<T> ShrdPtr<T>::operator=(ShrdPtr const &)
+  {
+    return ShrdPtr(this);
+  }
