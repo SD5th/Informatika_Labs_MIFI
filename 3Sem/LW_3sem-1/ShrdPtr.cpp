b605a6a2f9a336f1bca5c2ceda94adf81fa2f27d
SD5th <vlaka@ro.ru>
2024-11-05 02:00:12 +0300

    Fixed ShrdPtr(ShrdPtr const &)

--- 3Sem/LW_3sem-1/ShrdPtr.cpp
+++ 3Sem/LW_3sem-1/ShrdPtr.cpp
@@ -49,8 +49,7 @@
   template <class T>
   ShrdPtr<T>::ShrdPtr(ShrdPtr const & shrdPtr):
     ptr(shrdPtr.ptr),
-    referenceCount(shrdPtr.referenceCount),
-    isArray(shrdPtr.isArray) 
+    referenceCount(shrdPtr.referenceCount)
   {
     *referenceCount += 1;
   }
