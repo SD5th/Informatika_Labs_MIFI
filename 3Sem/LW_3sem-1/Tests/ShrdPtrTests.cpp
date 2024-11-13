cb1ee6ddc581b9da94096084619065a576011755
SD5th <vlaka@ro.ru>
2024-11-13 05:57:52 +0300

    Added tests for constructors

--- 3Sem/LW_3sem-1/Tests/ShrdPtrTests.cpp
+++ 3Sem/LW_3sem-1/Tests/ShrdPtrTests.cpp
@@ -2,14 +2,61 @@
 
 #include <catch2/catch_all.hpp>
 
-
-TEST_CASE("ShrdPtr: Constructors", "ShrdPtr")
+TEST_CASE("ShrdPtr: Constructors")
 {
-    SECTION("Empty constructor")
-    {
-        ShrdPtr<int> x;
-        REQUIRE(x.get() == nullptr);
-        //REQUIRE_THROWS()
-        //REQUIRE_NOTHROW()
-    }
+  SECTION("Constructor with nullptr")
+  {
+    ShrdPtr<int> ShrdPtr;
+    REQUIRE(ShrdPtr.get() == nullptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int(5); 
+    ShrdPtr<int> ShrdPtr(ptr);
+    REQUIRE(ShrdPtr.get() == ptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int(5); 
+    ShrdPtr<int> ShrdPtr1(ptr);
+    ShrdPtr<int> ShrdPtr2(ShrdPtr1);
+    REQUIRE(ShrdPtr2.get() == ptr);
+    REQUIRE(ShrdPtr1.getReferenceCount() == 2);
+    REQUIRE(ShrdPtr2.getReferenceCount() == 2);
+    ShrdPtr1.~ShrdPtr();
+    REQUIRE(ShrdPtr2.getReferenceCount() == 1);
+  }
+}
+
+
+
+
+TEST_CASE("ShrdPtr[]: Constructors")
+{
+  SECTION("Constructor with nullptr")
+  {
+    ShrdPtr<int[]> ShrdPtr;
+    REQUIRE(ShrdPtr.get() == nullptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int[3]{1, 2, 3}; 
+    ShrdPtr<int[]> ShrdPtr(ptr);
+    REQUIRE(ShrdPtr.get() == ptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int[3]{1, 2, 3}; 
+    ShrdPtr<int> ShrdPtr1(ptr);
+    ShrdPtr<int> ShrdPtr2(ShrdPtr1);
+    REQUIRE(ShrdPtr2.get() == ptr);
+    REQUIRE(ShrdPtr1.getReferenceCount() == 2);
+    REQUIRE(ShrdPtr2.getReferenceCount() == 2);
+    ShrdPtr1.~ShrdPtr();
+    REQUIRE(ShrdPtr2.getReferenceCount() == 1);
+  }
 }
--- 3Sem/LW_3sem-1/Tests/UnqPtrTests.cpp
+++ 3Sem/LW_3sem-1/Tests/UnqPtrTests.cpp
@@ -3,13 +3,37 @@
 #include <catch2/catch_all.hpp>
 
 
-TEST_CASE("UnqPtr: Constructors", "UnqPtr")
+TEST_CASE("UnqPtr: Constructors")
 {
-    SECTION("Empty constructor")
-    {
-        UnqPtr<int> x;
-        REQUIRE(1 == 1);
-        //REQUIRE_THROWS()
-        //REQUIRE_NOTHROW()
-    }
+  SECTION("Constructor with nullptr")
+  {
+    UnqPtr<int> UnqPtr;
+    REQUIRE(UnqPtr.get() == nullptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int(5); 
+    UnqPtr<int> UnqPtr(ptr);
+    REQUIRE(UnqPtr.get() == ptr);
+  }
+}
+
+
+
+
+TEST_CASE("UnqPtr[]: Constructors")
+{
+  SECTION("Constructor with nullptr")
+  {
+    UnqPtr<int[]> UnqPtr;
+    REQUIRE(UnqPtr.get() == nullptr);
+  }
+
+  SECTION("Constructor with given ptr")
+  {
+    int* ptr = new int[3]{1, 2, 3}; 
+    UnqPtr<int[]> UnqPtr(ptr);
+    REQUIRE(UnqPtr.get() == ptr);
+  }
 }
